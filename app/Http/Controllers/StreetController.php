d36fd8982d9ff1f78bc3c95ac1ee854577bbca03
Dao Thai Son <dao.thai.son@framgia.com>
2018-03-16 15:33:04 +0700

    undo prev commit

--- app/Http/Controllers/StreetController.php
+++ app/Http/Controllers/StreetController.php
@@ -37,4 +37,70 @@ class StreetController extends Controller
             'streets' => $streets,
         ]);
     }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Street  $street
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Street $street)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Street  $street
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Street $street)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Street  $street
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Street $street)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Street  $street
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Street $street)
+    {
+        //
+    }
 }
