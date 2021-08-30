df7cb2e88318a58be2855974491d8f3043532418
Dao Thai Son <49258854+sondt-1245@users.noreply.github.com>
2021-08-30 10:04:34 +0700

    Update create_multiple_db.sh

--- create_multiple_db.sh
+++ create_multiple_db.sh
@@ -6,7 +6,7 @@ set -u
 function create_user_and_database() {
 	local database=$1
 	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --host "$POSTGRES_HOST" <<-EOSQL
+	psql -v ON_ERROR_STOP=1 --user "$POSTGRES_USER" --host "$POSTGRES_HOST" <<-EOSQL
 	    CREATE DATABASE $database;
 	    GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_USER TO $database;
 EOSQL
